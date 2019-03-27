7fdf900676f4ca29d3021663525284732feb7034
沙云鹏 <shayunpeng0822@gmail.com>
2019-03-27 18:46:41 +0800

    change

--- src/main/java/com/sha/tool/AnalysisSettleExcel.java
+++ src/main/java/com/sha/tool/AnalysisSettleExcel.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AnalysisSettleExcel {
 
   public static void main(String[] args) {
-    System.out.println("输入东西");
+    System.out.println("输入东西2");
     Scanner sc = new Scanner(System.in);
     String s = sc.next();
     System.out.println(s);
