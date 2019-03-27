3c7b6d91bb74d4d1d8dda568162ff073beededbb
沙云鹏 <shayunpeng0822@gmail.com>
2019-03-27 18:39:59 +0800

    init

---
+++ src/main/java/com/sha/tool/AnalysisSettleExcel.java
@@ -0,0 +1,13 @@
+package com.sha.tool;
+
+import java.util.Scanner;
+
+public class AnalysisSettleExcel {
+
+  public static void main(String[] args) {
+    System.out.println("输入东西");
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    System.out.println(s);
+  }
+}
